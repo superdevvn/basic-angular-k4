402b92c0a0530977807d8c781e180e69782f7fb9
thanhhoa214 <thanhthanhhoa123@gmail.com>
2018-04-17 13:04:17 +0700

    Completed exercise 1

---
+++ Bai1-TypeScript/ThanhHoa/src/Functions.js
@@ -0,0 +1,6 @@
+var emp1 = new EmployerWithES5("Nguyen Van A", 1);
+document.getElementById('div1').innerHTML = emp1.say();
+document.getElementById('div2').innerHTML = emp1.think();
+var man1 = new ManagerWithES6();
+document.getElementById('div3').innerHTML = man1.say();
+document.getElementById('div4').innerHTML = man1.think('Boss');

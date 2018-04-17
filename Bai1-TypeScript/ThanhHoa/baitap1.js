d1c0104390937536d1e9d3c50ed9a404ab555fb7
thanhhoa214 <thanhthanhhoa123@gmail.com>
2018-04-17 12:36:42 +0700

    Nộp bài tập bữa đầu tiên

--- Bai1-TypeScript/ThanhHoa/baitap1.js
+++ Bai1-TypeScript/ThanhHoa/baitap1.js
@@ -1,6 +1,40 @@
-// Default Parameters
-// ES before ES6
-function logHello(name) {
-    console.log((name ? 'Hello ' + name : 'You forgot to fill name.'));
-}
-logHello('');
+var EmployerWithES5 = /** @class */ (function () {
+    function EmployerWithES5(name, id) {
+        // Mix string with value of variable
+        this.say = function () {
+            return 'My name is ' + this.name + '. I am a employer with id ' + this.id;
+        };
+        // Function Declared
+        this.think = function (target) {
+            return this.name + " will become " + (target ? target : 'Manager') + " in this year";
+        };
+        this.name = name;
+        this.id = id;
+    }
+    return EmployerWithES5;
+}());
+var ManagerWithES6 = /** @class */ (function () {
+    // Combine declare properties and constructor
+    function ManagerWithES6(name, id, sex) {
+        if (name === void 0) { name = 'Hoa'; }
+        if (id === void 0) { id = 4; }
+        var _this = this;
+        this.name = name;
+        this.id = id;
+        this.sex = sex;
+        // Template Literals
+        this.say = function () {
+            return "My name is " + _this.name + ". I am a manager with id " + _this.id;
+        };
+        // Arrow Function
+        // Default Parameters
+        this.think = function (target) {
+            if (target === void 0) { target = 'President'; }
+            return _this.name + " will become " + target + " in this year with ES6";
+        };
+        this.name = name;
+        this.id = id;
+        this.sex = sex || 'male';
+    }
+    return ManagerWithES6;
+}());
--- Bai1-TypeScript/ThanhHoa/baitap1.ts
+++ Bai1-TypeScript/ThanhHoa/baitap1.ts
@@ -1,7 +1,42 @@
 // Default Parameters
 // ES before ES6
-function logHello(name) {
-   console.log((name ? 'Hello ' + name : 'You forgot to fill name.'));
+interface People {
+   name: string;
+   id: number;
+   say(): string;
+   think(target: string): string;
 }
 
-logHello();
+class EmployerWithES5 implements People {
+   name: string;
+   id: number;
+   constructor(name: string, id: number) {
+      this.name = name;
+      this.id = id;
+   }
+   // Mix string with value of variable
+   say = function (): any {
+      return 'My name is ' + this.name + '. I am a employer with id ' + this.id;
+   };
+   // Function Declared
+   think = function (target?: string): string {
+      return `${this.name} will become ${target ? target : 'Manager'} in this year`;
+   }
+
+}
+
+class ManagerWithES6 implements People {
+   // Combine declare properties and constructor
+   constructor(public name: string = 'Hoa', public id: number = 4, public sex: string) {
+      this.name = name;
+      this.id = id;
+      this.sex = sex || 'male';
+   }
+   // Template Literals
+   say = (): string => {
+      return `My name is ${this.name}. I am a manager with id ${this.id}`;
+   };
+   // Arrow Function
+   // Default Parameters
+   think = (target: string = 'President') => `${this.name} will become ${target} in this year with ES6`;
+}

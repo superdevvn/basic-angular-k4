05b8a87e51894732c9890ba18d3ce9c30483049f
admin <phuongpro127@gmail.com>
2018-04-19 22:04:46 +0700

    te

---
+++ Bai1-TypeScript/Phuong/mjs/06-spread.js
@@ -0,0 +1,6 @@
+let courseweb = ["PHP", "Zend", "Wordpress"];
+let courseMobile = ["Androi", "java"];
+
+let course = ["HTML", "jQuery", ...courseweb, "Window Phone",...courseMobile];
+// "HTML", "jQuery", "Window Phone", "PHP", "Zend", "Wordpress", "Androi", "java"
+console.log(course);

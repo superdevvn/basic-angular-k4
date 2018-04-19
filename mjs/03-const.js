3ab4cf1f409c9d790b60fc98726523b8c08846b7
Quang Trang <huynhquangtrang1994@gmail.com>
2018-04-19 22:07:12 +0700

    remove code

--- mjs/03-const.js
+++
@@ -1,3 +0,0 @@
-const COURSE_NAME = "Typescript";
-
-console.log(COURSE_NAME);
--- mjs/06-spread.js
+++
@@ -1,6 +0,0 @@
-let courseweb = ["PHP", "Zend", "Wordpress"];
-let courseMobile = ["Androi", "java"];
-
-let course = ["HTML", "jQuery", ...courseweb, "Window Phone",...courseMobile];
-// "HTML", "jQuery", "Window Phone", "PHP", "Zend", "Wordpress", "Androi", "java"
-console.log(course);

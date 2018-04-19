109f4b2ea523258385f12341463ff845065c3979
Quang Trang <huynhquangtrang1994@gmail.com>
2018-04-19 20:20:29 +0700

    implement login form use bootstrap 4

---
+++ Bai2-Bootstrap/Code/src/app.ts
@@ -0,0 +1,19 @@
+declare let $;
+
+const onLoginFormSubmit = (event) => {
+    event.preventDefault();
+    
+    //JavaScript
+    const email1: any = document.getElementById('login-email');
+    const password1: any = document.getElementById('login-password');
+    console.log(email1, password1);
+    // alert(email.value + ': ' + password.value);
+
+    //jQuery
+    const email2: any = $('#login-email');
+    const password2: any = $('#login-password');
+    console.log(email2, password2);    
+    //alert(email.value + ': ' + password.value);
+
+    window.location.href = 'src/app.html'
+}

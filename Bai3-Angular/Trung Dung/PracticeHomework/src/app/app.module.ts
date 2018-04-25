2796c4bda0fa60e81e4e1da5d933668607321d61
Dung Nguyen <nguyentrungdung116@gmail.com>
2018-04-26 01:14:00 +0700

    update homework

---
+++ Bai3-Angular/Trung Dung/PracticeHomework/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms"
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './Login/Login.component';
+import { ProductComponent } from './product/product.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

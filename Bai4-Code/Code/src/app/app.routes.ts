6f2e49a8e2fa5bf810f9e6427e00ec27801bd6fd
Quang Trang <huynhquangtrang1994@gmail.com>
2018-05-04 19:23:20 +0700

    xoa code bai 4

--- Bai4-Code/Code/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/routes';
-import { DataListComponent } from './data-list/data-list.component';
-
-export const routes: Routes = [
-    { path :'list', component : DataListComponent},
-];
--- Bai4-Code/Code/src/pipes/number-pipes.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({name: 'number'})
-export class NumberPipe implements PipeTransform{
-
-    transform(value: any,...args: any[]){
-        return `${value.substring(1,value.length)} ${args[0]} `;
-    }
-}

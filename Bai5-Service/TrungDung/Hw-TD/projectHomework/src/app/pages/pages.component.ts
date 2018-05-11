e2636ba9cd5ad1109b0f293b2ca7cfa53c0d5503
Quang Trang <huynhquangtrang1994@gmail.com>
2018-05-11 19:38:40 +0700

    fix paging

--- Bai5-Service/TrungDung/Hw-TD/projectHomework/src/app/pages/pages.component.ts
+++ Bai5-Service/TrungDung/Hw-TD/projectHomework/src/app/pages/pages.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, Params } from '@angular/router';
-import { Http, Response } from '@angular/http'
+import { Router, ActivatedRoute, Params, NavigationEnd } from '@angular/router';
+import { Http, Response } from '@angular/http';
 import { Subscription } from 'rxjs/Subscription';
 @Component({
   selector: 'app-pages',
@@ -10,30 +10,30 @@ import { Subscription } from 'rxjs/Subscription';
 export class PagesComponent implements OnInit {
   page: number;
   movies: Array<object>;
-  
-  constructor(private http: Http, private route: ActivatedRoute) {
+
+  constructor(private http: Http, private route: ActivatedRoute, private router: Router) {
 
   }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.params['id'];
-    this.page = id;
-    this.loadMovies();
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const { page } = this.route.snapshot.params;
+        this.loadMovies(page);
+      }
+    });
   }
 
 
-  private async loadMovies() {
-    const sub = this.http.get('https://yts.am/api/v2/list_movies.json?page='+this.page).subscribe((response: Response) => {
+  private async loadMovies(page) {
+    const sub = this.http.get('https://yts.am/api/v2/list_movies.json?page=' + page).subscribe((response: Response) => {
       this.movies = response.json().data.movies;
-
-      const id = this.route.snapshot.params['id'];
-      console.log(id);
       sub.unsubscribe();
     }, error => {
       console.log(error);
       sub.unsubscribe();
     });
   }
-  
+
 
 }
